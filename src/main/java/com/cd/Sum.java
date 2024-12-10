e9b2046032e5cd563db744caf8f1eb20ce4b9b9b
Prashant Bhate <prashant.bhate@gmail.com>
2024-12-10 11:00:31 +0530

    Add simple unit test

---
+++ src/main/java/com/cd/Sum.java
@@ -0,0 +1,5 @@
+package com.cd;
+
+public interface Sum {
+    int sum(int a, int b);
+}
---
+++ src/test/java/com/cd/SumTest.java
@@ -0,0 +1,53 @@
+package com.cd;
+
+import org.junit.jupiter.api.*;
+
+import java.sql.SQLOutput;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SumTest {
+
+    Sum sumInstance;
+
+    @BeforeAll
+    static void init(){
+        System.out.println("init()");
+    }
+    @AfterAll
+    static void close(){
+        System.out.println("close()");
+    }
+
+    @BeforeEach
+    void setUp() {
+        sumInstance = new SumImpl();
+        System.out.println("SetUp");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("TearDown");
+    }
+
+    @Test
+    void testSumWithZero() {
+        System.out.println("testSumWithZero()");
+        int actual = sumInstance.sum(0,0);
+        assertEquals(0,actual,"0+0=0");
+    }
+
+    @Test
+    void testSumPositiveNumbers() {
+        System.out.println("testSumWithZero()");
+        int actual = sumInstance.sum(10,10);
+        assertEquals(20,actual,"10+10=20");
+    }
+
+    @Test
+    void testSumNegativeNumbers() {
+        System.out.println("testSumWithZero()");
+        int actual = sumInstance.sum(10,-10);
+        assertEquals(0,actual,"10-10=0");
+    }
+}
